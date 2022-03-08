eb81675793ad7d8d269be9c176ab7de03782872c
tederix <siava204520@mail.ru>
2022-03-08 16:25:28 +0300

    22.03.08
    Переработка T-p

--- WSP.py
+++ WSP.py
@@ -8,20 +8,57 @@ page = st.selectbox("Выберите страницу", ["p-T", "p-h", "p-s", "
 
 
 if page == "p-T":
-    p = st.number_input('Введите давление, МПа')
-    T = st.number_input('Введите температуру, °C')
+    p = st.number_input('Введите давление p, МПа')
+    T = st.number_input('Введите температуру T, °C')
+
+    col1, col2 = st.columns(2)
+
+    with col1:
+        st.write(""" p = """ + str('{:.6}'.format(p)) + """ МПа""")
+        st.write(""" T = """ + str('{:.6}'.format(T)) + """ °C""")
+        st.write("""  """)
+        st.write(""" h = """ + str('{:.6}'.format(IAPWS97(P=p, T=T + 273.15).h)) + """ кДж/кг""")
+        st.write(""" s = """ + str('{:.6}'.format(IAPWS97(P=p, T=T + 273.15).s)) + """ кДж/(кг*°C)""")
+        st.write("""  """)
+        st.write(""" v = """ + str('{:.6}'.format(IAPWS97(P=p, T=T + 273.15).v)) + """ м³/кг""")
+        st.write(""" ρ = """ + str('{:.6}'.format(IAPWS97(P=p, T=T + 273.15).rho)) + """ кг/м³""")
+        st.write(""" u = """ + str('{:.6}'.format(IAPWS97(P=p, T=T + 273.15).u)) + """ кДж/кг""")
+        st.write(""" cp = """ + str('{:.6}'.format(IAPWS97(P=p, T=T + 273.15).cp)) + """ кДж/(кг*°C)""")
+        st.write(""" cv = """ + str('{:.6}'.format(IAPWS97(P=p, T=T + 273.15).cv)) + """ кДж/(кг*°C)""")
+        st.write(""" λ = """ + str('{:.6}'.format(IAPWS97(P=p, T=T + 273.15).k)) + """ Вт/(м*°C)""")
+        st.write(""" μ = """ + str('{:.6}'.format(IAPWS97(P=p, T=T + 273.15).mu)) + """ Па*с""")
+        st.write(""" ν = """ + str('{:.6}'.format(IAPWS97(P=p, T=T + 273.15).nu)) + """ м²/с""")
+        st.write(""" Pr = """ + str('{:.6}'.format(IAPWS97(P=p, T=T + 273.15).Prandt)) + """""")
+        st.write("""  """)
+        st.write(""" w = """ + str('{:.6}'.format(IAPWS97(P=p, T=T + 273.15).w)) + """ м²/с""")
+        #st.write(""" k = """ + str('{:.6}'.format(IAPWS97(P=p, T=T + 273.15).gamma)) + """""")
+
+    with col2:
+        st.write(""" Давление """)
+        st.write(""" Температура """)
+        st.write("""  """)
+        st.write(""" Удельная энтальпия """)
+        st.write(""" Удельная энтропия """)
+        st.write("""  """)
+        st.write(""" Удельный объем """)
+        st.write(""" Плотность """)
+        st.write(""" Удельная внутренняя энергия """)
+        st.write(""" Удельная изобарная теплоемкость """)
+        st.write(""" Удельная изохорная теплоемкость """)
+        st.write(""" Коэф. теплопроводности """)
+        st.write(""" Динамическая вязкость """)
+        st.write(""" Кинематическая вязкость """)
+        st.write(""" Число Прандтля""")
+        st.write("""  """)
+        st.write(""" Скорость звука""")
+        #st.write(""" Коэф. изоэнтропы """)
 
 
-    st.write(""" p = """ + str('{:.6}'.format(p)) + """ МПа""")
-    st.write(""" T = """ + str('{:.6}'.format(T)) + """ °C""")
-    st.write("""  """)
-    st.write(""" h = """ + str('{:.6}'.format(IAPWS97(P=p, T=T+273.15).h)) + """ кДж/кг""")
-    st.write(""" s = """ + str('{:.6}'.format(IAPWS97(P=p, T=T+273.15).s)) + """ кДж/(кг*°C)""")
 
 
 if page == "p-h":
-    p = st.number_input('Введите давление, МПа')
-    h = st.number_input('Введите энтальпию, кДж/кг')
+    p = st.number_input('Введите давление p, МПа')
+    h = st.number_input('Введите энтальпию h, кДж/кг')
 
 
     st.write(""" p = """ + str('{:.6}'.format(p)) + """ МПа""")
@@ -33,8 +70,8 @@ if page == "p-h":
 
 
 if page == "p-s":
-    p = st.number_input('Введите давление, МПа')
-    s = st.number_input('Введите энтропию, кДж/(кг*°C)')
+    p = st.number_input('Введите давление p, МПа')
+    s = st.number_input('Введите энтропию s, кДж/(кг*°C)')
 
     st.write(""" p = """ + str('{:.6}'.format(p)) + """ МПа""")
     st.write(""" s = """ + str('{:.6}'.format(s)) + """ кДж/(кг*°C)""")
@@ -45,8 +82,8 @@ if page == "p-s":
 
 
 if page == "h-s":
-    h = st.number_input('Введите энтальпию, кДж/кг')
-    s = st.number_input('Введите энтропию, кДж/(кг*°C)')
+    h = st.number_input('Введите энтальпию h, кДж/кг')
+    s = st.number_input('Введите энтропию s, кДж/(кг*°C)')
 
     st.write(""" h = """ + str('{:.6}'.format(h)) + """ кДж/кг""")
     st.write(""" s = """ + str('{:.6}'.format(s)) + """ кДж/(кг*°C)""")
@@ -57,8 +94,8 @@ if page == "h-s":
 
 
 if page == "p-x":
-    p = st.number_input('Введите давление, МПа')
-    x = st.number_input('Введите степень сухости, %')
+    p = st.number_input('Введите давление p, МПа')
+    x = st.number_input('Введите степень сухости x, %', min_value=0.0, max_value=100.0)
 
     st.write(""" p = """ + str('{:.6}'.format(p)) + """ МПа""")
     st.write(""" x = """ + str('{:.6}'.format(x)) + """ %""")
@@ -69,8 +106,8 @@ if page == "p-x":
 
 
 if page == "T-x":
-    T = st.number_input('Введите температуру, °C')
-    x = st.number_input('Введите степень сухости, %')
+    T = st.number_input('Введите температуру T, °C')
+    x = st.number_input('Введите степень сухости x, %', min_value=0.0, max_value=100.0)
 
     st.write(""" T = """ + str('{:.6}'.format(T)) + """ °C""")
     st.write(""" x = """ + str('{:.6}'.format(x)) + """ %""")
